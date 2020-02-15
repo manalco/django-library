91d06b522cae92ed0007045d7913676ea82075a3
Manuel Alvarado <manalco2@gmail.com>
2020-02-15 14:05:23 -0500

    Books app

---
+++ books/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .models import Subject
+
+
+class Book(models.Model):
+	name = models.CharField(max_length=255)
+	description = models.TextField(max_length=2000)
+	author = models.CharField(max_length=255)
+	stock = models.PositiveIntegerField(default=2, validators=[MinValueValidator(1)]) # Always leave a copy on the library
+
+	def __str__(self):
+		return self.name
