118dc8af4c63e6fafae75b96f270d7bd251d9714
Manuel Alvarado <manalco2@gmail.com>
2020-02-15 14:38:27 -0500

    Fix Book model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .models import Subject
+from django.core.validators import MinValueValidator
 
 
 class Book(models.Model):
--- django_library/settings.py
+++ django_library/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'subjects',
+    'books',
+    'loans',
 ]
 
 MIDDLEWARE = [
