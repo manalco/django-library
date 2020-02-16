3f81803aa6a91d2fef6eda073e6ea2e87d5d79b7
Manuel Alvarado <manalco2@gmail.com>
2020-02-15 20:34:20 -0500

    loan updates

---
+++ README.md
@@ -0,0 +1,6 @@
+# Future Work
+
+
+* Implement `Autocomplete` widget for Loans: Student and Book fields.
+* Implement `Autocomplete` widget for Books: Author field.
+* Add Book Cover field for books.
--- loans/models.py
+++ loans/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
 
+
 class Loan(models.Model):
-	notes = models.TextField(max_length=4000, null=True)
-	subject = models.ForeignKey('subjects.Subject', related_name='loans', on_delete=models.CASCADE)
+	student = models.ForeignKey(User, related_name='loans', on_delete=models.CASCADE, limit_choices_to={'groups__name': "Student"})
 	book = models.ForeignKey('books.Book', null=True, related_name='loans', on_delete=models.CASCADE)
-	loan_start = models.DateField(default=date.today)
+	subject = models.ForeignKey('subjects.Subject', null=False, related_name='loans', on_delete=models.CASCADE)
+	loan_starts = models.DateField(default=date.today)
 	loan_ends = models.DateField(default=date.today)
-	student = models.ForeignKey(User, related_name='loans', on_delete=models.CASCADE)
-	created_by = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE)
+	notes = models.TextField(max_length=4000, null=True)
