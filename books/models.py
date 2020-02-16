b8eecff3973cae7f32ef9ae8f92edbecaa3ed714
Manuel Alvarado <manalco2@gmail.com>
2020-02-16 16:26:16 -0500

    loan data flow and restrictions

--- books/models.py
+++ books/models.py
@@ -4,9 +4,9 @@ from django.core.validators import MinValueValidator
 
 class Book(models.Model):
 	name = models.CharField(max_length=255)
-	description = models.TextField(max_length=2000)
 	author = models.CharField(max_length=255)
+	description = models.TextField(max_length=2000)
 	stock = models.PositiveIntegerField(default=2, validators=[MinValueValidator(1)]) # Always leave a copy on the library
 
 	def __str__(self):
-		return self.name
+		return '['+str(self.stock)+'] '+self.name
--- loans/admin.py
+++ loans/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Loan
 
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    readonly_fields = ["return_date"]
 
-admin.site.register(Loan)
+
+##admin.site.register(Loan)
